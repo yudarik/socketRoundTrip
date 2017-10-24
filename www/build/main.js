02bde9c0ca4f2beab73e491fe69a580a3716d42c
Arik Yudin <yudarik@gmail.com>
2017-10-24 23:37:32 +0300

    socketRoundTrip - update README

--- www/build/main.js
+++ www/build/main.js
@@ -160,7 +160,7 @@ var MyApp = (function () {
     }
     MyApp.prototype.ngOnInit = function () {
         this.serverAddressForm = this.fb.group({
-            address: this._window.location.toString().replace(/\/$/, '') || "http://127.0.0.1:8080"
+            address: this._window.location.toString().replace(/\/$/, '') || "https://socket-round-trip.herokuapp.com"
         });
         this.connected = false;
     };
